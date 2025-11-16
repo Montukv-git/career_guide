5d33ace5801e9b83a23b417cfe70973b06d0b303
Montukeshwar Vaishnaw <montukeshwar20@gmail.com>
2025-11-16 20:55:56 +0530

    Clean up settings.py by removing comments
    Removed commented sections for database, password validation, internationalization, and static files.

--- career_guide/career_guide/settings.py
+++ career_guide/career_guide/settings.py
@@ -60,20 +60,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'career_guide.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -90,22 +82,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
